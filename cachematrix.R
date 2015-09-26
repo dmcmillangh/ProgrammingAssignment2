55cd1f622829f33a1a1856a882b092b53071bb07
dmcmillangh <dmcmillan@mcmconsult.com>
2015-09-25 21:25:38 -0500

    Code now works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## First function creates a list of functions, performing no real work
+## expect a matrix, square, and not ill-conditioned
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL ##matrix(NULL,nrow=nrow(x),ncol(x))
+  
+##  print(inv)
+    set <- function(y=matrix()) {
+        x <<- y
+        inv <<- NULL ##matrix(NULL,nrow=nrow(x),ncol(x))
+    }
+    get <- function() x
+  
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+  
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
+## use cacheSolve() instead of native function solve(), as it caches the solution at the first run, 
+## merely retrieving it in subsequent calls
 
-## Write a short comment describing this function
+## this function decides whether to do the real work, 
+## or just retreive work (calc'ing the inverse) already done
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    inv<-x$getinv()
+  ## Return a matrix that is the inverse of 'x'
+  ## First see if we already have it  
+    if(!is.null(inv)){
+      message("retrieving Inverse")
+      return (inv)
+  }
+## otherwise, calculate it
+  xmat<-x$get()
+  inv<-solve(xmat)
+  x$setinv(inv)
+  return(inv)
+  }
